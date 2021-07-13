3984c3e6c323c9812145dbe0cbdddc209f9c0351
Ps1h0 <platonov.liceum@mail.ru>
2021-07-13 20:57:39 +0800

    Initial commit

---
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -0,0 +1,15 @@
+create table products (id bigserial primary key, name varchar(255), cost int);
+insert into products (name, cost) values
+('A', 10),
+('B', 20),
+('C', 30),
+('D', 40),
+('E', 50),
+('F', 60),
+('G', 70),
+('H', 80),
+('I', 90),
+('J', 100),
+('K', 110),
+('L', 120),
+('M', 130);
